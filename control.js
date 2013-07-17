f88bf6402bc09845f27acccd4cbd80f6f1147f8a
huntinggirled <huntinggirled@gmail.com>
2013-07-17 12:34:23 +0900

    update.

---
+++ control.js
@@ -0,0 +1,15 @@
+(function() {
+  jQuery("#comment").keypress(function(ev) {
+    if ((ev.which && ev.which === 13) ||
+        (ev.keyCode && ev.keyCode === 13)) {
+      alert('13');
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+jQuery('textarea#comment').focus(function() {
+  alert('aaa');
+});
+})();
