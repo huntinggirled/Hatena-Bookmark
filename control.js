26a19e11b5cd05d8dd255cec0bb155afe2ee7577
huntinggirled <huntinggirled@gmail.com>
2013-07-17 12:47:15 +0900

    update.

--- control.js
+++ control.js
@@ -1,15 +1,21 @@
 (function() {
-  jQuery("#comment").keypress(function(ev) {
-    if ((ev.which && ev.which === 13) ||
-        (ev.keyCode && ev.keyCode === 13)) {
-      alert('13');
-      return false;
-    } else {
-      return true;
-    }
-  });
+//   jQuery("#comment").keypress(function(ev) {
+//     if ((ev.which && ev.which === 13) ||
+//         (ev.keyCode && ev.keyCode === 13)) {
+//       alert('13');
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   });
 
-jQuery('textarea#comment').focus(function() {
-  alert('aaa');
-});
+// jQuery('textarea#comment').focus(function() {
+//   alert('aaa');
+// });
+document.onkeypress = enter;
+    function enter(){
+        if( window.event.keyCode == 13 ){
+            return false;
+        }
+    }
 })();
